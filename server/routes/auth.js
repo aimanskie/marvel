728582865537cd7f3800e66c62f4eba04213be04
aimanskie <aiman@assohwah.com>
2023-01-26 21:41:35 +0800

    hidden files remove

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,14 +2,12 @@ import express from 'express'
 
 const router = express.Router()
 
-// controllers
 const {
   signup,
   signin,
   forgotPassword,
   resetPassword,
   userUpdate,
-  // checkSignIn,
   getUser,
 } = require('../controllers/auth')
 
@@ -18,8 +16,6 @@ router.post('/signin', signin)
 router.post('/forgot-password', forgotPassword)
 router.post('/reset-password', resetPassword)
 router.put('/update-user', userUpdate)
-router.get('/get-user', /* checkSignIn */ getUser)
-// router.put('/authenticate', renewToken)
-// router.get('/authenticate', checkToken)
+router.get('/get-user', getUser)
 
 export default router
